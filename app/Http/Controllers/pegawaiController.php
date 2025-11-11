c9899bdd9132bba84c8d670aeb55e83758f23f57
Zallxupuu <zallrpl@gmail.com>
2025-11-11 13:31:21 +0700

    tugas sekolah backend laravel, frontend reactJS

---
+++ app/Http/Controllers/pegawaiController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\pegawai;
+use Illuminate\Http\Request;
+
+class pegawaiController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data = pegawai::all();
+        return response()->json($data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request ->validate([
+            'namaPegawai' => 'required | min:3 ',
+            'alamatPegawai' => 'required| min:3',
+            'umurPegawai' => 'numeric | required | max:2'
+        ]);
+
+        $data = pegawai::create($request->all());
+
+        return response()->json('$data');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ app/Models/pegawai.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class pegawai extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'namaPegawai',
+        'alamatPegawai',
+        'umurPegawai'
+    ];
+}
